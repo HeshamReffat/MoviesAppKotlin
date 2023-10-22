6d090f7336e2b8e1404df360055f931ef8891e15
HishamReffat <hisham.reffat@e-motionegypt.com>
2023-10-22 12:15:55 +0300

    init dependency

---
+++ app/src/main/java/com/hisham/moviesapp/data/Movie.kt
@@ -0,0 +1,15 @@
+package com.hisham.moviesapp.data
+
+import com.google.gson.annotations.SerializedName
+
+data class Movie(
+    val id: Int,
+    @SerializedName("overview")
+    val overview: String,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    @SerializedName("title")
+    val title: String,
+)
